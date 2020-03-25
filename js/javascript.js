97c98b76575df5171651a54e7c35816d524f70bf
Rasmus Olfert Dohm <rasm945b@edu.campusvejle.dk>
2020-03-25 11:31:12 +0100

    something is wrong

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
+window.onload = function() {
 if ('loading' in HTMLImageElement.prototype) {
     const images = document.querySelectorAll('img[loading="lazy"]');
     images.forEach(img => {
       img.src = img.dataset.src;
+      console.log("hi")
     });
   } else {
     // Dynamically import the LazySizes library
@@ -9,4 +11,4 @@ if ('loading' in HTMLImageElement.prototype) {
     script.src =
       'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
     document.body.appendChild(script);
-}
+}}
